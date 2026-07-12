058606bfce745be63416602db293b31d5099af1f
grandinettirita-creator <grandinetti.rita@gmail.com>
2026-07-12 13:39:03 +0200

    Update commenti.js

--- widget-commenti/widget-commenti/commenti.js
+++ widget-commenti/widget-commenti/commenti.js
@@ -1,1 +1,24 @@
+document.getElementById("commentForm").addEventListener("submit", function(event){
 
+    event.preventDefault();
+
+    const nome = document.getElementById("nome").value;
+    const messaggio = document.getElementById("messaggio").value;
+
+    const lista = document.getElementById("listaCommenti");
+
+    if(lista.innerHTML.includes("Non ci sono ancora commenti.")){
+        lista.innerHTML = "";
+    }
+
+    lista.innerHTML += `
+        <div class="commento">
+            <strong>${nome}</strong><br>
+            <p>${messaggio}</p>
+            <hr>
+        </div>
+    `;
+
+    document.getElementById("commentForm").reset();
+
+});
