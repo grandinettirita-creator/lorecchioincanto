f913a7f2a79710dddbddfff0153ca6d58569abae
grandinettirita-creator <grandinetti.rita@gmail.com>
2026-07-22 18:14:45 +0200

    Update salva-commento.js

--- netlify/functions/salva-commento.js
+++ netlify/functions/salva-commento.js
@@ -1,1 +1,22 @@
+exports.handler = async (event) => {
 
+  if (event.httpMethod !== "POST") {
+    return {
+      statusCode: 405,
+      body: "Metodo non consentito"
+    };
+  }
+
+  const dati = JSON.parse(event.body);
+
+  console.log("Nuovo commento ricevuto:", dati);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      success: true,
+      messaggio: "Commento ricevuto."
+    })
+  };
+
+};
